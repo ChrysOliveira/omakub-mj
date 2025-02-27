418e4dc6ee7c5f6f24f9de14b8db3e267a1c4ddb
Chrystian Oliveira <chrystianmoliveira7@gmail.com>
2025-02-27 19:53:43 -0300

    removing all related to LazyNvim and themes update to nvim

--- install/terminal/app-neovim.sh
+++ install/terminal/app-neovim.sh
@@ -2,22 +2,14 @@ yay -S --needed neovim --noconfirm
 
 # Only attempt to set configuration if Neovim has never been run
 if [ ! -d "$HOME/.config/nvim" ]; then
-  # Use LazyVim
+  # Use AtroNvim
   git clone https://github.com/ChrysOliveira/nvim.git ~/.config/nvim
-
-  # Disable update notification popup in starter config
-  # removing this config that was related to LazyVim
-  # sed -i 's/checker = { enabled = true }/checker = { enabled = true, notify = false }/g' ~/.config/nvim/lua/config/lazy.lua
 
   # Make everything match the terminal transparency
   cp ~/.local/share/omakub/configs/neovim/transparency.lua ~/.config/nvim/plugins/
 
   # Default to Tokyo Night theme
   cp ~/.local/share/omakub/themes/tokyo-night/neovim.lua ~/.config/nvim/lua/plugins/theme.lua
-
-  # Enable default extras
-  # removing this config that was related to LazyVim
-  # cp ~/.local/share/omakub/configs/neovim/lazyvim.json ~/.config/nvim/lazyvim.json
 fi
 
 # Replace desktop launcher with one running inside Alacritty

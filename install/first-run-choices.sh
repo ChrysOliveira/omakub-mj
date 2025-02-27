25cb467d49956a6698d6d6b5bcb6a172d9c7345a
Chrystian Oliveira <chrystianmoliveira7@gmail.com>
2025-02-27 19:33:21 -0300

    fix default choices

--- install/first-run-choices.sh
+++ install/first-run-choices.sh
@@ -1,4 +1,4 @@
-OPTIONAL_APPS=("1password","Bitwarden","Discord","Dropbox","Rubymine","Steam","Windows","Zoom","Audacity","Brave","Doom-emacs","Obs-studio","Spotify","Virtualbox","Zed")
+OPTIONAL_APPS=("1password" "Bitwarden" "Discord" "Dropbox" "Rubymine" "Steam" "Windows" "Zoom" "Audacity" "Brave" "Doom-emacs" "Obs-studio" "Spotify" "Virtualbox" "Zed")
 DEFAULT_OPTIONAL_APPS='Bitwarden,Spotify,Discord,Steam,Obs-studio'
 export OMAKUB_FIRST_RUN_OPTIONAL_APPS=$(gum choose "${OPTIONAL_APPS[@]}" --no-limit --selected $DEFAULT_OPTIONAL_APPS --height 7 --header "Select optional apps" | tr ' ' '-')
 

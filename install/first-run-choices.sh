d2b1a15168a752b23f60a9e72405f6a521b5331e
Chrystian Oliveira <chrystianmoliveira7@gmail.com>
2025-02-27 19:31:21 -0300

    updating default choices in gum

--- install/first-run-choices.sh
+++ install/first-run-choices.sh
@@ -1,11 +1,11 @@
-OPTIONAL_APPS=("Bitwarden" "Spotify" "Zoom" "Discord" "Dropbox")
-DEFAULT_OPTIONAL_APPS='Bitwarden,Spotify'
+OPTIONAL_APPS=("1password","Bitwarden","Discord","Dropbox","Rubymine","Steam","Windows","Zoom","Audacity","Brave","Doom-emacs","Obs-studio","Spotify","Virtualbox","Zed")
+DEFAULT_OPTIONAL_APPS='Bitwarden,Spotify,Discord,Steam,Obs-studio'
 export OMAKUB_FIRST_RUN_OPTIONAL_APPS=$(gum choose "${OPTIONAL_APPS[@]}" --no-limit --selected $DEFAULT_OPTIONAL_APPS --height 7 --header "Select optional apps" | tr ' ' '-')
 
 AVAILABLE_LANGUAGES=("Ruby on Rails" "Node.js" "Go" "PHP" "Python" "Elixir" "Rust" "Java")
-SELECTED_LANGUAGES="Ruby on Rails","Node.js"
+SELECTED_LANGUAGES="Go","Node.js","Python","Rust","Java"
 export OMAKUB_FIRST_RUN_LANGUAGES=$(gum choose "${AVAILABLE_LANGUAGES[@]}" --no-limit --selected "$SELECTED_LANGUAGES" --height 10 --header "Select programming languages")
 
 AVAILABLE_DBS=("MySQL" "Redis" "PostgreSQL")
-SELECTED_DBS="PostgreSQL,Redis"
+SELECTED_DBS="PostgreSQL,Redis","MySQL"
 export OMAKUB_FIRST_RUN_DBS=$(gum choose "${AVAILABLE_DBS[@]}" --no-limit --selected "$SELECTED_DBS" --height 5 --header "Select databases (runs in Docker)")

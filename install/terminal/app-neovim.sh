b142a0a0b2be6a4395e5ff63b981b26e5cfc9ca3
Chrystian Oliveira <chrystianmoliveira7@gmail.com>
2025-02-27 20:06:53 -0300

    fix  path plugins lua

--- install/terminal/app-neovim.sh
+++ install/terminal/app-neovim.sh
@@ -6,7 +6,7 @@ if [ ! -d "$HOME/.config/nvim" ]; then
   git clone https://github.com/ChrysOliveira/nvim.git ~/.config/nvim
 
   # Make everything match the terminal transparency
-  cp ~/.local/share/omakub/configs/neovim/transparency.lua ~/.config/nvim/plugins/
+  cp ~/.local/share/omakub/configs/neovim/transparency.lua ~/.config/nvim/lua/plugins/
 
   # Default to Tokyo Night theme
   cp ~/.local/share/omakub/themes/tokyo-night/neovim.lua ~/.config/nvim/lua/plugins/theme.lua

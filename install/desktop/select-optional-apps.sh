2ae7002a2e962e83c723c6c8cac66973d92f27d6
Chrystian Oliveira <chrystianmoliveira7@gmail.com>
2025-02-27 21:13:32 -0300

    fix

--- install/desktop/select-optional-apps.sh
+++ install/desktop/select-optional-apps.sh
@@ -2,7 +2,7 @@ if [[ -v OMAKUB_FIRST_RUN_OPTIONAL_APPS ]]; then
 	apps=$OMAKUB_FIRST_RUN_OPTIONAL_APPS
 
 	if [[ -n "$apps" ]]; then
-		for single_app in ${(f)app}; do
+		for single_app in ${(f)apps}; do
     		source "$OMAKUB_PATH/install/desktop/optional/app-${single_app:l}.sh"
 		done
 	fi
